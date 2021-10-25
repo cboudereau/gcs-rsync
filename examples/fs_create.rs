1d457e25b86199acc2b9bd6fbf4830f408a0d6fa
cboudereau <cboudereau@gmail.com>
2021-10-25 20:25:12 +0200

    clean

--- examples/fs_create.rs
+++
@@ -1,7 +0,0 @@
-use tokio::io::AsyncWriteExt;
-
-#[tokio::main]
-async fn main() {
-    let mut fs = tokio::fs::File::create("test.txt").await.unwrap();
-    fs.write_all(b"hello").await.unwrap();
-}
